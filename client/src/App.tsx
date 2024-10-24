d8ad98fef6b168e367a0ecc47c3f8807df0c44e1
ATHARVA-JADHAV <atharvajadhav550@gmail.com>
2024-10-24 21:51:23 +0530

    Sign in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/index";
 import Stock from "./components/stock";
-
+import Signup from "./components/Signup";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Signup />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/stock" element={<Stock />} />
       </Routes>
     </Router>
